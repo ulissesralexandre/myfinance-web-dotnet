66fe76af474ef7d84395b286270ccdc20fd3046a
Ulisses R. Alexandre <78184745+ulissesralexandre@users.noreply.github.com>
2024-01-19 21:23:11 -0300

    Entity Framework

---
+++ myfinance-web-dotnet-domain/Entities/PlanoConta.cs
@@ -0,0 +1,9 @@
+﻿namespace myfinance_web_dotnet_domain;
+
+public class PlanoConta
+{
+    public int? Id {get; set;}
+    public string Descricao {get; set;}
+    public string Tipo {get; set;}
+
+}
---
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain;
+
+namespace myfinance_web_dotnet_infra;
+
+public class MyFinanceDbContext : DbContext
+{
+    public DbSet <PlanoConta> PlanoConta {get; set;}
+
+    public DbSet <Transacao> Transacao {get; set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;");
+    }
+}
